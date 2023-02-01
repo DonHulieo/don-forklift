cdeb1d2b9d5befa6be20e2d35406d244278ebe1d
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-02-01 19:54:48 +1100

    Updated Native Name

--- client/main.lua
+++ client/main.lua
@@ -39,7 +39,7 @@ local function DrawText3Ds(x, y, z, text)
         SetTextEntry("STRING")
         SetTextCentre(1)
         AddTextComponentString(text)
-        DrawText(_x, _y)
+        EndTextCommandDisplayText(_x, _y)
         local factor = (string.len(text)) / 370
         DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 41, 11, 41, 100)
     end
